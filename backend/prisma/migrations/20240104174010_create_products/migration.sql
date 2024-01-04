3fb16b2499192ea97d79589e603a7af5192f7a25
Matheus Santos <maatheus.santos223@gmail.com>
2024-01-04 16:28:19 -0300

    feat: add Product to prisma schema

---
+++ backend/prisma/migrations/20240104174010_create_products/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_code_key" ON "products"("code");
