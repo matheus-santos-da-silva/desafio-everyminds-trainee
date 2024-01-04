4666774d063241e69fd9774c6b5293132501a81a
Matheus Santos <maatheus.santos223@gmail.com>
2024-01-04 16:28:52 -0300

    feat: add Product entity

---
+++ backend/src/entities/product.ts
@@ -0,0 +1,46 @@
+import { Decimal } from '@prisma/client/runtime/library';
+
+interface ProductProps {
+  name: string
+  code: string
+  description: string
+  price: Decimal
+} 
+
+export class Product {
+
+  private props: ProductProps;
+
+  get name() {
+    return this.props.name;
+  }
+  get code() {
+    return this.props.code;
+  }
+  get price() {
+    return this.props.price;
+  }
+  get description() {
+    return this.props.description;
+  }
+
+  set name(value: string) {
+    this.props.name = value;
+  }
+
+  set code(value: string) {
+    this.props.code = value;
+  }
+
+  set description(value: string) {
+    this.props.description = value;
+  }
+
+  set price(value: Decimal) {
+    this.props.price = value;
+  }
+  
+  constructor(props: ProductProps) {
+    this.props = props;
+  }
+}
