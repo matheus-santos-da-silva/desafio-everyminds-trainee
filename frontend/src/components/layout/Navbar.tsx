ef90bf3d49fef4f3bf97c08a4a6ed4283b13db82
Matheus Santos <maatheus.santos223@gmail.com>
2024-01-07 11:56:58 -0300

    feat: create Navbar

---
+++ frontend/src/components/layout/Navbar.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'; 
+
+import styles from './Navbar.module.css';
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_logo}>
+        <img src='https://nunessports.com/cdn/shop/files/logo_ns-removebg-preview_1_75x@2x.png?v=1698633524' alt="logo-nunes-sports"/>
+        <h2>Nunes Sports</h2>
+      </div>
+      <ul>
+        <li>
+          <Link to={'/'}>Início</Link>
+        </li>
+        <li>
+          <Link to={'/create'}>Criar Produto</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
