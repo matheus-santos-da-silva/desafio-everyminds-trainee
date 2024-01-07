cb45e4156053956eeefcf3f2d46afb043b3a79e1
Matheus Santos <maatheus.santos223@gmail.com>
2024-01-07 11:57:12 -0300

    feat: create Footer

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span className="bold">Nunes Sports</span> 2024
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
