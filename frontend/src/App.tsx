43cda58fc6b55a9e532c71d0055a34f9e0834c90
Matheus Santos <maatheus.santos223@gmail.com>
2024-01-09 19:07:13 -0300

    feat: add Edit Page and CreateProduct Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Container from './components/layout/Container';
+import Message from './components/layout/Message';
 
 /* Pages */
 import Home from './components/pages/Home';
@@ -11,16 +12,18 @@ import CreateProduct from './components/pages/CreateProduct';
 
 /* Context */
 import { ProductProvider } from './context/ProductContext';
+import EditProduct from './components/pages/EditProduct';
 
 function App() {
   return (
     <Router>
       <ProductProvider>
         <Navbar/>
+        <Message/>
         <Container >
           <Routes>
-            <Route path='/edit' />
-            <Route path='/create'element={<CreateProduct/>}/>
+            <Route path='/products/edit/:id' element={<EditProduct/>}/>
+            <Route path='/products/create'element={<CreateProduct/>}/>
             <Route path='/'element={<Home/>}/>
           </Routes>
         </Container>
