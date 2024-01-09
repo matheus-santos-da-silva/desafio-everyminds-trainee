532f8ee55b797cfab591e874b36ba72609b36f42
Matheus Santos <maatheus.santos223@gmail.com>
2024-01-09 13:00:06 -0300

    feat: change the logo of navbar to a Link

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'; 
-
+'https://nunessports.com/cdn/shop/files/logo_ns-removebg-preview_1_75x@2x.png?v=1698633524';
 import styles from './Navbar.module.css';
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
-        <img src='https://nunessports.com/cdn/shop/files/logo_ns-removebg-preview_1_75x@2x.png?v=1698633524' alt="logo-nunes-sports"/>
+        <Link to='/'><img src='https://nunessports.com/cdn/shop/files/logo_ns-removebg-preview_1_75x@2x.png?v=1698633524'alt='Nunes-Sports-Logo'></img></Link>
         <h2>Nunes Sports</h2>
       </div>
       <ul>
